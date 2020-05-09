a96f469554196c6460d1472a1658aad7d0708428
Wojtaszek91 <spiochtar@o2.pl>
2020-05-09 14:36:24 +0200

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        public static int Fib(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            return Fib(n - 1) + Fib(n - 2);
+        }
+        public static void Main()
+        {
+            int parsedValue;
+            bool check = true;
+            while (check)
+            {
+                Console.WriteLine("With number of Fibonacci do you want count ?");
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out parsedValue) && parsedValue > 0)
+                {
+                    Console.WriteLine($"Your Fibonacci number is  {Fib(parsedValue)}");
+                    check = false;
+                }
+                else Console.WriteLine("Put in positive number");
+            }
+        }
+    }
+}
